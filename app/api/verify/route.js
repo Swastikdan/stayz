6aff654ccfe3bafad899de83e19923a1c0754d4f
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-05-28 13:17:17 +0530

    stripe 4

--- app/api/verify/route.js
+++ app/api/verify/route.js
@@ -1,57 +1,41 @@
-import { NextResponse, NextRequest } from "next/server";
 import stripe from "@/utils/stripe";
-
-// Custom middleware to get raw body
-const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-async function getRawBody(req) {
-  return new Promise((resolve, reject) => {
-    let data = "";
-
-    req.on("data", (chunk) => {
-      data += chunk;
-    });
-
-    req.on("end", () => {
-      resolve(data);
-    });
-
-    req.on("error", (error) => {
-      reject(error);
-    });
-  });
-}
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
 
 export async function POST(request) {
-  const sig = request.headers.get('stripe-signature');
+  const body = await request.text();
+  const endpointSecret = process.env.STRIPE_SECRET_WEBHOOK_KEY;
+  const sig = headers().get("stripe-signature");
   let event;
 
-  // Get raw body
-  const rawBody = await getRawBody(request);
-
   try {
-    event = stripe.webhooks.constructEvent(rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
   } catch (err) {
-    console.log(`⚠️  Webhook signature verification failed. ${err.message}`);
-    return NextResponse.json({ received: false, error: err.message });
+    console.log(`Webhook Error: ${err}`);
+    return new Response(`Webhook Error: ${err}`, {
+      status: 400,
+    });
   }
 
-  if (event.type === 'checkout.session.completed') {
-    const session = event.data.object;
+  console.log(`Event: ${JSON.stringify(event)}`);
 
-    // Print session data
-    console.log(`💰 Payment received! ${session}`);
-
-    // Here you can retrieve the customer email and other session details
-    const customerEmail = session.customer_details.email;
-    console.log(`Customer email: ${customerEmail}`);
+  switch (event.type) {
+    case "checkout.session.async_payment_failed":
+      const checkoutSessionAsyncPaymentFailed = event.data.object;
+      console.log(`checkout.session.async_payment_failed: ${JSON.stringify(checkoutSessionAsyncPaymentFailed)}`);
+      break;
+    case "checkout.session.async_payment_succeeded":
+      const checkoutSessionAsyncPaymentSucceeded = event.data.object;
+      console.log(`checkout.session.async_payment_succeeded: ${JSON.stringify(checkoutSessionAsyncPaymentSucceeded)}`);
+      break;
+    case "checkout.session.completed":
+      const checkoutSessionCompleted = event.data.object;
+      console.log(`checkout.session.completed: ${JSON.stringify(checkoutSessionCompleted)}`);
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
   }
-
-  return NextResponse.json({ received: true });
+  return new Response("RESPONSE EXECUTE", {
+    status: 200,
+  });
 }
-
-export { config };
