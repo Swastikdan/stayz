2138f851b652ee3003e9750e6e262ccf294abd7f
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-05-28 13:05:32 +0530

    stripe 3

--- app/api/verify/route.js
+++ app/api/verify/route.js
@@ -36,22 +36,21 @@ export async function POST(request) {
   try {
     event = stripe.webhooks.constructEvent(rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET);
   } catch (err) {
-    console.log(`⚠️  Webhook Error: ${err.message}`);
-    return NextResponse.json({ received: false, error: err.message }).status(400);
+    console.log(`⚠️  Webhook signature verification failed. ${err.message}`);
+    return NextResponse.json({ received: false, error: err.message });
   }
 
-  // Handle the event
-  console.log(`Unhandled event type ${event.type}`);
-
-  // Here you can retrieve the customer email and other session details
   if (event.type === 'checkout.session.completed') {
     const session = event.data.object;
-    const customerEmail = session.customer_details.email;
+
+    // Print session data
     console.log(`💰 Payment received! ${session}`);
+
+    // Here you can retrieve the customer email and other session details
+    const customerEmail = session.customer_details.email;
     console.log(`Customer email: ${customerEmail}`);
   }
 
-  // Return a 200 response to acknowledge receipt of the event
   return NextResponse.json({ received: true });
 }
 
